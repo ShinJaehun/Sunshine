dd52ca902851f0287a40a634476ea86e49e8f2a3
ShinJaehun <gunsmoke.shin@gmail.com>
2015-10-08 05:12:51 +0900

    Using Sunshine Service instead of AsyncTask

--- app/src/main/java/com/shinjaehun/sunshine/ForecastFragment.java
+++ app/src/main/java/com/shinjaehun/sunshine/ForecastFragment.java
@@ -1,5 +1,8 @@
 package com.shinjaehun.sunshine;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -21,6 +24,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.shinjaehun.sunshine.data.WeatherContract;
+import com.shinjaehun.sunshine.service.SunshineService;
 
 //import java.sql.Time;
 
@@ -227,9 +231,21 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         //String temp_units = prefs.getString(getString(R.string.pref_temperature_key), getString(R.string.pref_temperature_default));
         //Log.v(LOG_TAG, "temp  : " + temp_units);
 
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+//        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
+//        String location = Utility.getPreferredLocation(getActivity());
+//        weatherTask.execute(location);
+
+        Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
+        alarmIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
+
+        PendingIntent pi = PendingIntent.getBroadcast(getActivity(), 0, alarmIntent, PendingIntent.FLAG_ONE_SHOT);
+        AlarmManager am = (AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);
+
+//        Intent intent = new Intent(getActivity(), SunshineService.class);
+//        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
+//        getActivity().startService(intent);
+
     }
 
 //    @Override
